579b55abf394529983a679741dd2326d91dcb3dc
Kauã <kauarodriguesmarciano@gmail.com>
2026-06-09 09:17:48 -0300

    implementa ChatController com enponitPOST /api/chat

--- ChatBotEasyComp/Controllers/ChatController.cs
+++ ChatBotEasyComp/Controllers/ChatController.cs
@@ -1,6 +1,25 @@
-﻿namespace ChatBotEasyComp.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using ChatBotEasyComp.Services;
+
+namespace ChatBotEasyComp.Controllers
 {
-    public class ChatController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ChatController : ControllerBase
     {
+        private readonly OpenAIService _openAIService;
+
+        public ChatController(OpenAIService openAIService)
+        {
+            _openAIService = openAIService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Chat([FromBody] string pergunta)
+        {
+            var resposta = await _openAIService.GetResponse(pergunta);
+
+            return Ok(resposta);
+        }
     }
 }
